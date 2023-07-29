587aefd7d6792a8aed7e9f7f0026a8fffe3feb29
Achyut Thapa <achyut777thapa@gmail.com>
2023-07-29 16:13:44 +0545

    fffff

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+// import Footer from "./components/Footer";
 
 const App = () => {
   return (
@@ -12,7 +12,7 @@ const App = () => {
       <About />
       <Work />
       <Contact />
-      <Footer />
+      {/* <Footer /> */}
     </>
   );
 };
