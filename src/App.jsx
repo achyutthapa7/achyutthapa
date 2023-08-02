aaa07beb053e8bac21918d0b791092aa95a05f6a
Achyut Thapa <achyut777thapa@gmail.com>
2023-08-02 09:31:04 +0545

    changesmade

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Home from "./components/Home";
 import About from "./components/About";
 import Work from "./components/Work";
 import Contact from "./components/Contact";
-// import Footer from "./components/Footer";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
